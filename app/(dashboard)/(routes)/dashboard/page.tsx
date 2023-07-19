9f67034b7e66565d864dffda0495bdc50904ed82
Aman Sahu <92854825+AmanKrSahu@users.noreply.github.com>
2023-07-19 19:18:38 +0530

    Clerk Authentication (#3)

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
     return (
         <div>
             <p>Dashboard Page</p>
+            <UserButton afterSignOutUrl="/"/>
         </div>
     )
 }
