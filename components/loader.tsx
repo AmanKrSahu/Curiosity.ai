e0abf861cf60273819e352ba3aa4998200e5ec47
Aman <amankrsahu003@gmail.com>
2023-07-25 16:18:09 +0530

    Added Conversation AI API

---
+++ components/loader.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+
+export const Loader = () => {
+    return(
+        <div
+            className="
+                h-full
+                flex
+                flex-col
+                gap-y-4
+                items-center
+                justify-center
+            "
+        >
+            <div
+                className="
+                    w-10
+                    h-10
+                    relative
+                    animate-spin
+                "
+            >
+                <Image
+                    alt="logo"
+                    fill
+                    src="/Images/loader.png"
+                />
+            </div>
+            <p className="text-sm text-muted-foreground">
+                Rendering your request
+            </p>
+        </div>
+    )
+}
