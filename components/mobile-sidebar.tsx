ddb775121514c6e32d85fec1b3c0fb28942f0dd4
Aman <amankrsahu003@gmail.com>
2023-07-26 14:26:34 +0530

    Added API limit UI Counter

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -7,7 +7,13 @@ import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
 
-const MobileSidebar = () => {
+interface MobileSidebarProps{
+    apiLimitCount: number;
+}
+
+const MobileSidebar = ({
+    apiLimitCount = 0
+}: MobileSidebarProps) => {
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
@@ -26,7 +32,7 @@ const MobileSidebar = () => {
                 </Button>
             </SheetTrigger>
             <SheetContent side="left" className="p-0">
-                <Sidebar/>
+                <Sidebar apiLimitCount={apiLimitCount}/>
             </SheetContent>
         </Sheet>
     )
