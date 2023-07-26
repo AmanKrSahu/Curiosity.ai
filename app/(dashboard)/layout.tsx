3ca6f2b8e48c4dd08403558ae1913eb28ba25420
Aman <amankrsahu003@gmail.com>
2023-07-26 13:53:24 +0530

    Added API limit UI Counter

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,14 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
     children
 }: {
     children: React.ReactNode
 }) => {
+    const apiLimitCount = await getApiLimitCount();
+
     return(
         <div
             className="h-full relative">
@@ -22,7 +25,7 @@ const DashboardLayout = ({
                     bg-gray-900
                 "
             >
-                <Sidebar/>
+                <Sidebar apiLimitCount={apiLimitCount}/>
             </div>
             <main className="md:pl-72">
                 <Navbar/>
